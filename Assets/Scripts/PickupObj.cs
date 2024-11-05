60efc925c7c78bba18fc2e9d716a5de9cc44ea88
Titanovsky <titanovskyteam@gmail.com>
2024-11-06 01:09:01 +0500

    add IUsed in PickupObj and add obj in scene

--- Assets/Scripts/PickupObj.cs
+++ Assets/Scripts/PickupObj.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickupObj : MonoBehaviour
+public class PickupObj : MonoBehaviour, IUsed   
 {
+    public void Use()
+    {
+        Debug.Log("dsas");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
