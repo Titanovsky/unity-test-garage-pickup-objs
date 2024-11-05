3976561431dd13e3e467c9db69507c0db49496eb
Titanovsky <titanovskyteam@gmail.com>
2024-11-06 00:27:08 +0500

    add PlayerMovement and PlayerMouse

--- Assets/Scripts/Player/PlayerMouse.cs
+++ Assets/Scripts/Player/PlayerMouse.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerMouse : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _minAngleX = -90f; // for realistic look around
+    [SerializeField] private float _maxAngleX = 90f;
+    [SerializeField] private float _sens = 300f;
+
+    private float _mouseX;
+    private float _mouseY;
+    private float _rotationX = 0f;
+    private float _rotationY = 90f; // чтобы сразу смотрел на дверь
+
+    public new Camera camera;
+
+    private void Look()
     {
-        
+        _mouseX = Input.GetAxis("Mouse X") * _sens * Time.deltaTime;
+        _mouseY = Input.GetAxis("Mouse Y") * _sens * Time.deltaTime;
+
+        _rotationX -= _mouseY;
+        _rotationX = Mathf.Clamp(_rotationX, _minAngleX, _maxAngleX);
+
+        _rotationY += _mouseX;
+
+        if (camera != null)
+        {
+            camera.transform.rotation = Quaternion.Euler(_rotationX, _rotationY, 0);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void PrepareLook()
     {
-        
+        if (camera == null)
+            camera = GetComponentInChildren<Camera>(true);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Start()
+    {
+        PrepareLook();
+    }
+
+    private void Update()
+    {
+        Look();
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(PlayerMouse))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private CharacterController _char;
+    [SerializeField] private PlayerMouse _mouse;
+
+    [SerializeField] private float _speed = 25f;
+    private Vector3 _dir;
+
+    private void PrepareMovement()
     {
-        
+        if (_char == null)
+            _char = GetComponent<CharacterController>();
+
+        if (_mouse == null)
+            _mouse = GetComponent<PlayerMouse>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CheckInputs()
     {
-        
+        Vector3 forward = _mouse.camera.transform.forward;
+        Vector3 right = _mouse.camera.transform.right;
+
+        // каждый раздельно в случае одновременного нажатия
+        if (Input.GetKey(KeyCode.W)) // не люблю switch, но здесь он подойдёт отлично
+            _dir += forward;
+
+        if (Input.GetKey(KeyCode.S))
+            _dir += -forward;
+
+        if (Input.GetKey(KeyCode.D))
+            _dir += right;
+
+        if (Input.GetKey(KeyCode.A))
+            _dir += -right;
+    }
+
+    private void Move() // по сути можно туда накинуть аргумент, и сделать более масштабируемый контроллер, но не в этот раз
+    {
+        _char.Move(_dir * _speed * Time.deltaTime);
+
+        _dir = Vector3.zero;
+    }
+
+
+    private void Start()
+    {
+        PrepareMovement();
+    }
+
+    private void Update()
+    {
+        CheckInputs();
+        Move();
     }
 }
